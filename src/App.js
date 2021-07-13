45ef8419a73f0d99b8843ba42c9de663084eb9b8
sudheer madasu <madasu.sudheer7@gmail.com>
2021-07-13 22:57:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import HeaderContent from "./headerContent";
+
+function App() {
+  return (
+    <div>
+      <HeaderContent />
+    </div>
+  );
+}
+
+export default App;
